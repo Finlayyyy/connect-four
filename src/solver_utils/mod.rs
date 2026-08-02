ed5c4e47fd68feab1fb53d1d4cec13d4c2b97494
finlaymaltby <finlay3.141@gmail.com>
2026-08-02 23:17:33 +1000

    improved entry

--- src/solver_utils/mod.rs
+++ src/solver_utils/mod.rs
@@ -3,7 +3,7 @@ pub mod move_sorter;
 pub mod position;
 pub mod solver_manager;
 
-pub use cache::{Cache, BoundType};
+pub use cache::Cache;
 pub use move_sorter::MoveSorter;
 pub use position::{Position, WithInfo};
 pub use solver_manager::*;
